a0bb6f290bcc9ef781b331d6a82304e1452ce162
Kangning Huang <fytaso@gmail.com>
2021-11-29 11:24:58 -0700

    Update urban_land_setup.R

--- functions/urban_land_setup.R
+++ functions/urban_land_setup.R
@@ -1,7 +1,12 @@
 library(sf)
+library(dplyr)
 library(raster)
 library(spData)
+library(rnaturalearth)
 
 # Set working directory to path of the script
 setwd(dirname(getSourceEditorContext()$path))
 
+countries <- ne_download(scale = 10, returnclass = 'sf')
+
+countries %>% filter(ADM0_A3=='USA') %>% st_geometry() %>% plot()
