1a3cf587f3e490fcdc13eada91ef0b43b484163e
Kangning Huang <fytaso@gmail.com>
2021-11-29 13:39:22 -0700

    update urban land quantities

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # URBANMOD-ZIPF
 This repository contains codes and sample data to run the urban land expansion model that preserves the zipf-law of urban cluster sizes--URBANMO-ZIPF. Detailed descriptions of this model can be found in Huang et al (2019).
 
-## How to run the demo of URBANMOD-ZIPF
+## How to run URBANMOD-ZIPF
 Open "main.m" in Matlab.
+
+## Urban land areas
+The areas of urban land (km^2) for each country under five Shared Socioeconomic Pathways (SSPs) are stored in "results/urban_land.csv". These areas are calculated based on the panel regression coefficients listed in the Supplementary Table 2 in Huang et al (2019). To perform the calculation, run "functions/urban_land_quantities.R" in R.
 
 ## System requirements
 - Matlab R2021b or above;
--- functions/urban_land_quantities.R
+++ functions/urban_land_quantities.R
@@ -1,6 +1,7 @@
 library(readr)
 library(dplyr)
 library(tidyr)
+library(tibble)
 library(rstudioapi)
 
 # Set working directory to path of the script
@@ -10,7 +11,11 @@ setwd(dirname(getSourceEditorContext()$path))
 tbl_iso_reg32 <- readr::read_csv(file.path('..', 'data', 'ISO3_countryname_32regions.csv'), show_col_types = F)
 
 # Coefficients of panel regression: Urban Land per capita (km2 / mil persons) ~ GDP per capita ($ / person)
-tbl_coef <- readr::read_csv(file.path('..', 'data', 'ulc_vs_gdpc_32regions.csv'), show_col_types = F)
+tbl_coef <- readr::read_csv(file.path('..', 'data', 'ulc_vs_gdpc_32regions.csv'), show_col_types = F) %>%
+  tibble::column_to_rownames('Coefficients')
+
+coef_intercept <- tbl_coef['Intercept', 'Estimate']
+coef_gdpc      <- tbl_coef['GDPC',      'Estimate']
 
 # Shared Socioeconomic Pathways (SSPs) forecasts of countries from IIASA
 tbl_ssps <- readr::read_csv(file.path('..', 'data', 'SspDb_country_data_2013-06-12.csv'), show_col_types = F)
@@ -33,7 +38,8 @@ tbl_pop_urb <- tbl_ssps %>%
   dplyr::mutate(year = as.integer(year)) %>%
   dplyr::filter(year>=2030 & year<=2100 & year%%10==0) %>%
   merge(tbl_pop, by=c('SCENARIO', 'REGION', 'year')) %>%
-  dplyr::mutate(urb_pop = population * urban_share / 100)
+  dplyr::mutate(urban_pop = population * urban_share / 100) %>%
+  dplyr::select(-population)
 
 # Filter GDP (billion US$)
 tbl_gdp <- tbl_ssps %>%
@@ -46,6 +52,18 @@ tbl_gdp <- tbl_ssps %>%
 
 # Calculate GDP per capita ($ / person)
 tbl_gdpc <- merge(tbl_gdp, tbl_pop, by = c('SCENARIO', 'REGION', 'year')) %>%
-  dplyr::mutate(GDPC = (GDP * 10^9) / (population * 10^6))
+  dplyr::mutate(GDPC = (GDP * 10^9) / (population * 10^6)) %>%
+  dplyr::select(-GDP, -population)
 
 # Calculate Urban Land (km2)
+tbl_urban_land <- tbl_gdpc %>% 
+  merge(tbl_iso_reg32, by.x='REGION', by.y='ISO') %>%
+  merge(tbl_coef %>% rownames_to_column('REGION'), by='REGION') %>%
+  dplyr::mutate(ULC = GDPC * coef_gdpc + Estimate + coef_intercept) %>%
+  merge(tbl_pop_urb, by = c('SCENARIO', 'REGION', 'year')) %>%
+  dplyr::mutate(urban_land = as.integer(ULC * urban_pop)) %>%
+  dplyr::select(-Estimate, -urban_share)
+
+# Output Urban Land (km2)
+tbl_urban_land %>%
+  readr::write_csv(file.path('..', 'results', 'urban_land.csv'))
