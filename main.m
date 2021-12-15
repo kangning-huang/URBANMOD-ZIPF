a909d988ffbfcf3fd05b1d64e1ab8c0b6ff895bb
Kangning Huang <fytaso@gmail.com>
2021-12-15 15:12:42 -0700

    update main.m

--- main.m
+++ main.m
@@ -5,15 +5,20 @@ addpath("utils/");
 % Read urban land areas
 ul_areas = readtable("results/urban_land.csv");
 
-% Subset urban land areas in 2050 under SSP5
-ul_areas_2050_ssp5 = ul_areas(strcmp(ul_areas.SCENARIO,'SSP5') & ul_areas.year==2050,:);
-
 % Number of runs in Monte Carlo simulation
 ntimes = 100;
 
 % Read model parameters
-country  = 'CHN'; % Change here to run in another country/region
-ul_areas = ul_areas_2050_ssp5(strcmp(ul_areas_2050_ssp5.REGION, country),:);
+% Change here to run in another country/region, scenario, year
+country  = 'CHN'; 
+scenario = 'SSP5';
+year     = 2050;
+% Subset urban land areas
+ul_areas_sub = ul_areas(strcmp(ul_areas.SCENARIO,scenario) & ul_areas.year==year,:);
+ul_areas = ul_areas_sub(strcmp(ul_areas_sub.REGION, country),:);
 year     = ul_areas.year;
 ssp      = ul_areas.SCENARIO;
 ul_area  = ul_areas.urban_land;
+
+path = fullfile('results/', country);
+urbanmod_prof_new(path, year_start, year_end, scenario, nurban, nntimes)
