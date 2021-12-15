722b8b3ee79a6edba82708f3827ee3490b16e508
Kangning Huang <fytaso@gmail.com>
2021-12-15 07:50:37 -0700

    update main.m

--- main.m
+++ main.m
@@ -2,4 +2,13 @@
 addpath("functions/");
 addpath("utils/");
 
-% 
+% Read urban land areas
+ul_areas = readtable("results/urban_land.csv");
+
+% Subset urban land areas in 2100 under SSP5
+ul_areas_2100_ssp5 = ul_areas(strcmp(ul_areas.SCENARIO,'SSP5') & ul_areas.year==2100,:);
+
+% Run URBANMOD in China, India, and United Stated
+ul_areas_chn =  ul_areas_2100_ssp5(strcmp(ul_areas_2100_ssp5.REGION,'CHN'),:);
+ul_areas_ind =  ul_areas_2100_ssp5(strcmp(ul_areas_2100_ssp5.REGION,'IND'),:);
+ul_areas_usa =  ul_areas_2100_ssp5(strcmp(ul_areas_2100_ssp5.REGION,'USA'),:);
