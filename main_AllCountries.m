4a284e538f25ab3776475807d2af01ede783698d
Kangning Huang <fytaso@gmail.com>
2022-03-21 16:17:58 -0400

    AFG

--- main_AllCountries.m
+++ main_AllCountries.m
@@ -4,12 +4,13 @@ addpath("utils/");
 
 % ntimes is the number of runs in Monte Carlo simulation, if ntimes is
 % large, the simulations will be VERY SLOW on personal computers
-ntimes = 100;
+ntimes = 10;
 
 % Loop through all scenarios and countries
 urban_land = readtable("results/urban_land.csv");
 for i = 1:size(urban_land,1)
     country  = urban_land{i,'REGION'}{1};
     scenario = urban_land{i,'SCENARIO'}{1};
+    urbanmod_prob_new(country, scenario, ntimes);
 end
 
