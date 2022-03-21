0acb36f7aa8325515312f800d2e2ecee040ae33e
Kangning Huang <fytaso@gmail.com>
2022-03-21 11:39:42 -0400

    update main

---
+++ main_AllCountries.m
@@ -0,0 +1,14 @@
+% Add necessary functions to paths
+addpath("functions/");
+addpath("utils/");
+
+% ntimes is the number of runs in Monte Carlo simulation, if ntimes is
+% large, the simulations will be VERY SLOW on personal computers
+ntimes = 100;
+
+% Loop through all scenarios and countries
+urban_land = readtable("results/urban_land.csv");
+for i = 1:size(urban_land,1)
+    country  = urban_land{i,'REGION'}{1};
+    scenario = urban_land{i,'SCENARIO'}{1};
+end
